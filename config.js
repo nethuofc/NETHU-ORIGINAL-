bc266601499d3a9a87db029af27d21b526c5e4c2
nethuofc <nethumax311@gmail.com>
2024-09-10 22:11:49 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "jbpgSSxI#Ov-eUBXsnU3mtZllvMyc-7eS6jpSDuW_0-xb3LV6i9s",
+SESSION_ID: process.env.SESSION_ID || "fDwjzTiR#vE3lq_MFhoi6Hp5LAC732zRgJH74SyfpOho6gl-eTOe",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/7f0d7a04a30a602307e3d.jpg",
 SUDO_NB: process.env.SUDO_NB || "ඔයාගෙ owner number එක දෙන්න",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "privet",
 AUTO_VOICE: "true"
 };
